83c5298276c7dff1adb20f9e0b0b5b65f1523dc2
Tapio Ikkala <tapio.ikkala@relex.fi>
2017-06-19 17:51:31 +0300

    #20 Ekat layer testit skulaa

--- app/javascript/Components/__tests__/Layer-test.jsx
+++ app/javascript/Components/__tests__/Layer-test.jsx
@@ -23,18 +23,51 @@ describe("Layer", () => {
         mountedLayer = undefined;
     });
 
-    it("", () => {});
+    beforeEach(() => {
+        props = {
+            categories: undefined,
+            layer: undefined,
+            publications: undefined
+        };
+        mountedLayer = undefined;
+    });
 
-// ^boilerplate code that is run before each test
+    // ^boilerplate code that is run before each test
 
-/*    it("always renders a ListGroupItem", () => {
-        beforeEach(() => {
-            props.dropdownOpen = false;
-            props.categorie = ['Test', 'Category'];
-            props.id = 1;
-        })
-        const listGroupItems = layer().find("ListGroupItem");
+    it("always renders a tr (table row)", () => {
+        props.categories = [];
+        props.layer = {
+            id: 1,
+            name: "Test"
+        }
+        props.publications = [];
+        const listGroupItems = layer().find("tr");
         expect(listGroupItems.length).toBeGreaterThan(0);
-    });*/
+    });
+
+    describe("the rendered tr", () => {
+        it("contains everything else that is rendered", () => {
+            props.categories = [];
+            props.layer = {
+                id: 1,
+                name: "Test"
+            }
+            props.publications = [];
+            const trs = layer().find("tr")
+            const wrappingTr = trs.first();
+            expect(wrappingTr.children()).toEqual(layer().children());
+        });
+        it("contains ButtonDropdown", () => {
+            props.categories = [];
+            props.layer = {
+                id: 1,
+                name: "Test"
+            }
+            props.publications = [];
+        })
+    });
+
+
+
 
 });
