fa75d182bf5cc4573333d4e0be848732bc7b6734
Tapio Ikkala <tapio.ikkala@relex.fi>
2017-06-26 20:10:15 +0300

    #34 Boilerplate code for the Publication component

--- app/javascript/Components/Publication/Publication.js
+++ app/javascript/Components/Publication/Publication.js
@@ -2,6 +2,21 @@
 import React, { Component } from 'react';
 
 class Publication extends Component{
+
+    constructor() {
+        super();
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            modalOpen: false
+        };
+    }
+
+    toggle() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
+
     render(){
         return null
     }
