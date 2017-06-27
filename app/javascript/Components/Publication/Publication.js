69716bc198b0fbb6399897f4dba27afe867e2f8c
Tapio Ikkala <tapio.ikkala@relex.fi>
2017-06-27 18:31:25 +0300

    #34 Show title of the publication as modal title

--- app/javascript/Components/Publication/Publication.js
+++ app/javascript/Components/Publication/Publication.js
@@ -18,12 +18,12 @@ class Publication extends Component {
         });
     }
 
-    rener() {
+    render() {
         return (
             <div>
-                <span onClick={this.toggle}>{this.props.publication.name}</span>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
+                <span onClick={this.toggle}>{this.props.pub.name}</span>
+                <Modal isOpen={this.state.modalOpen} toggle={this.toggle} className={this.props.className}>
+                    <ModalHeader toggle={this.toggle}>{this.props.pub.name}</ModalHeader>
                     <ModalBody>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </ModalBody>
