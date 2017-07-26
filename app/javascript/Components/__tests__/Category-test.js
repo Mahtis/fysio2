fcafa3b9e7a79f841570debf9064624d7e429e6a
Tapio Ikkala <tapio.ikkala@relex.fi>
2017-07-26 19:05:18 +0300

    #50 Test coverage of Category.js to 100 %, also modified the category list so that it stays open even though one category is selected

--- app/javascript/Components/__tests__/Category-test.js
+++ app/javascript/Components/__tests__/Category-test.js
@@ -1,33 +1,108 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 import Category from '../Fysio/LayerList/Layer/CategoryList/Category/Category.js';
 
-test('Category spans the category name', () => {
-    let c = {
-        id: 1,
-        name: "Dog"
+describe("Category", () => {
+    let props;
+    let mountedCategory;
+    const category = () => {
+        if (!mountedCategory) {
+            mountedCategory = mount(
+                <Category {...props} />
+            );
+        }
+        return mountedCategory;
     }
-    const categoryComponent = shallow(
-        <Category category={c} />
-    );
 
-    let text = categoryComponent.find('span');
+    beforeEach(() => {
+        props = {
+            category: undefined,
+            name: undefined,
+            layer: undefined,
+            updateTable: undefined,
+            status: undefined
+        };
+        mountedCategory: undefined
+    });
 
-    expect(text.text()).toEqual('Dog');
+    const select = sinon.stub();
 
+    it("always renders one DropDownItem", () => {
+        initializeProps(props);
+        const dropDownItem = category().find("DropdownItem");
+        expect(dropDownItem.length).toBe(1);
+    });
+
+    describe("the rendered DropDownItem", () => {
+
+        it("contains everything else that gets rendered", () => {
+            initializeProps(props);
+            const wrappingDropDownItem = category().find("DropdownItem");
+            expect(wrappingDropDownItem.children()).toEqual(category().children())
+        });
+
+        it("renders one span", () => {
+            initializeProps(props);
+            const wrappingDropDownItem = category().find("DropdownItem");
+            expect(wrappingDropDownItem.find("span").length).toBe(1);
+        });
+
+        it("calls select function once when clicked", () => {
+            initializeProps(props);
+            const wrapper = category();
+            // instance of the wrapper is needed to call functions
+            const instance = wrapper.instance();
+            // let's mock the select() function as we want to test if it gets called
+            instance.select = sinon.spy(instance.select);
+            wrapper.update();
+            // select is not called if the DropdownItem is not clicked
+            expect(instance.select.notCalled).toEqual(true)
+            wrapper.find("DropdownItem").simulate("click");
+            expect(instance.select.calledOnce).toEqual(true);
+        });
+
+        it("has btn as className if status is false", () => {
+            // status is initialized to false
+            initializeProps(props);
+            const wrapper = category().find("DropdownItem");
+            expect(wrapper.hasClass("btn")).toEqual(true);
+        });
+
+        it("does not have selected as className if status is false", () => {
+            // status is initialized to false
+            initializeProps(props);
+            const wrapper = category().find("DropdownItem");
+            expect(wrapper.hasClass("selected")).toEqual(false);
+        });
+
+        it("has btn and selected as ClassName if status is true", () => {
+            initializeProps(props);
+            const wrapper = category();
+            wrapper.setProps({status: true});
+            expect(wrapper.find("DropdownItem").hasClass("selected")).toEqual(true);
+            expect(wrapper.find("DropdownItem").hasClass("btn")).toEqual(true);
+        });
+
+        describe("the rendered span", () => {
+
+            it("has name of the category as text", () => {
+                initializeProps(props);
+                const span = category().find("span");
+                expect(span.text()).toEqual("EEG")
+            });
+        });
+    });
 });
 
-test('Category no name', () => {
-    let c = {
+
+function initializeProps(props) {
+    props.category = {
         id: 1,
-        name: ''
-    }
-    const categoryComponent = shallow(
-        <Category category={c} />
-    );
-
-    let text = categoryComponent.find('span');
-
-    expect(text).exists;
-    
-});
+        name: "EEG",
+        layer_id: 1,
+    };
+    props.name = props.category.name;
+    props.satus = false;
+    props.updateTable = function mockUpdate(name) { return name;}
+}
