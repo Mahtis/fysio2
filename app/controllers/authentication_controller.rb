38fb31e0e66d550af89d1c724a20c629afdebac1
Antti Häkli <antti.hakli@helsinki.fi>
2017-08-24 18:03:00 +0300

    Prevent logging in as normal user if user is oauth user

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,19 +1,32 @@
 class AuthenticationController < ApplicationController
-  #skip_before_action :authenticate_request
+  # skip_before_action :authenticate_request
 
   def authenticate
-    command = AuthenticateUser.call(params[:name], params[:password])
-
-    if command.success?
-      cookies['auth_token'] = command.result
-      render json: { auth_token: command.result, user: User.find_by_name(params[:name]) }
+    if params[:password] == 'salasana'
+      render json: { error: "invalid password" }, status: :unauthorized
     else
-      render json: { error: command.errors }, status: :unauthorized
+      command = AuthenticateUser.call(params[:name], params[:password])
+      if command.success?
+        cookies['auth_token'] = command.result
+        render json: { auth_token: command.result, user: User.find_by_name(params[:name]) }
+      else
+        render json: { error: command.errors }, status: :unauthorized
+      end
     end
   end
 
   def create_oauth
     callback = request.env['omniauth.auth']
+    unless User.find_by_name(callback.info.nickname)
+      user = User.new
+      user.name = callback.info.nickname
+      user.realname = callback.info.name
+      user.email = callback.info.email
+      user.role = 'inactive'
+      user.oauth = 'gh'
+      user.password_digest = 'salasana'
+      user.save
+    end
     payload = {:user => callback.info.nickname}
     cookies['auth_token'] = JsonWebToken.encode(payload)
     redirect_to root_path
--- app/javascript/Components/NavBar/NavBar.js
+++ app/javascript/Components/NavBar/NavBar.js
@@ -4,7 +4,6 @@ import LayerLink from './LayerLink/LayerLink.js';
 import PropTypes from 'prop-types';
 
 import { Collapse, Button, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, NavbarHeader } from 'reactstrap';
-import { Link } from 'react-router-link'
 
 /**
  * Navigation bar component, contains all the components within navigation bar
@@ -33,7 +32,6 @@ class NavBar extends Component{
      */
 
     componentDidMount() {
-        console.log(this.props.layerTypes.length);
         if(this.props.layerTypes.length > 0) {
             this.setState({
                 text: this.props.layerTypes[0].name
@@ -73,7 +71,7 @@ class NavBar extends Component{
 
         let addPubButton = (
             <NavItem>
-                <NavLink className="btn modeButtons" href="/publications/new">Create Publication</NavLink>
+                <NavLink className={"btn modeButtons"} href="/publications/new">Create Publication</NavLink>
             </NavItem>
         );
         let loginButton = (
@@ -81,13 +79,19 @@ class NavBar extends Component{
                 <Button className={"modeButtons"} onClick={this.props.doLogout}>Logout</Button>
             </NavItem>
         );
-        if(this.props.userMode === "guest"){
+        let statusText = (<span />);
+
+        if(this.props.userMode === "guest" || this.props.userMode === undefined){
             addPubButton = null;
             loginButton=(
                 <NavItem>
                     <Button className={"modeButtons"} onClick={this.props.toLogin}>Login</Button>
                 </NavItem>
             );
+        }
+
+        if(this.props.userMode !== undefined){
+            statusText= (<Button className={"modeButtons"}>{this.props.userMode} </Button>);
         }
 
 
@@ -110,57 +114,10 @@ class NavBar extends Component{
                 </Nav>
                 <Collapse isOpen={this.state.isOpen} navbar>
                 </Collapse>
-                {this.props.userMode}
+                {statusText}
                 <Nav className="ml-auto" navbar>
                     {loginButton}
                 </Nav>
-            </Navbar>
-        );
-
-        let oldVersion = (
-            <Navbar color="faded" light toggleable className={"navBar"}>
-                <NavbarBrand className="header" href="/">fysio</NavbarBrand>
-                <NavbarToggler right onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto nav-left" navbar>
-                        {layerTypes.map(t => <LayerLink className="navbar-left" key={t.id} type={t} changeView={this.changeView} />)}
-                    </Nav>
-                    <Nav className="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink href="#">Home</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="#">Users</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        );
-
-        let routerVersion = (
-            <Navbar color="faded" light toggleable className={"navBar"}>
-                <NavbarToggler right onClick={this.toggle} />
-
-                <Nav >
-                    <NavItem><Link to="/"><Button className={"modeButtons"} >fysio</Button></Link></NavItem>
-                    <NavItem><Link to="/about"><Button className={"modeButtons"} >about</Button></Link></NavItem>
-                    <NavItem><Link to="/login"><Button className={"modeButtons"} >login</Button></Link></NavItem>
-
-                    <NavItem><NavLink href="/"><Button className={"modeButtons"} >fysio</Button></NavLink></NavItem>
-                    <NavItem><NavLink tag={Link} href="/about">about</NavLink></NavItem>
-                    <NavItem><NavLink href="/login"><Button className={"modeButtons"} >login</Button></NavLink></NavItem>
-                </Nav>
-
-                <Collapse isOpen={this.state.isOpen} navbar>
-                </Collapse>
-
-                <Nav className="ml-auto nav-left" navbar>
-                    {layerTypes.map(t => <LayerLink className="navbar-left" key={t.id} type={t} changeView={this.changeView} />)}
-                </Nav>
-
-                <Collapse isOpen={this.state.isOpen} navbar>
-                </Collapse>
-
             </Navbar>
         );
 
