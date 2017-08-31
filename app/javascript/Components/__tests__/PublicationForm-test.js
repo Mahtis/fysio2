ddd304861b80489de1e77f67e345a520f8ceffbd
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2017-08-31 15:09:31 +0300

    save

--- app/javascript/Components/__tests__/PublicationForm-test.js
+++ app/javascript/Components/__tests__/PublicationForm-test.js
@@ -1,3 +1,45 @@
-/**
- * Created by perkoila on 31.8.2017.
- */
+import React from "react";
+import { shallow } from "enzyme";
+import PublicationForm from '../NavBar/PublicationForm';
+import {Tooltip} from 'reactstrap';
+import TestHelper from '../Helpers/Tests.js';
+import Data from '../../Services/Data';
+
+describe("PublicationForm", () => {
+    let props;
+    let mountedForm;
+    let formProps = props = {
+        createPublication: function(){},
+        createCategory: function (){},
+        data: TestHelper.newData(new Data())
+
+    };
+    const pubForm = () => {
+        mountedForm = TestHelper.initializationWithMount(mountedForm, <PublicationForm {...props} />);
+        return mountedForm;
+    };
+
+    beforeEach(() => {
+        props = {
+            createPublication: function(){},
+            createCategory: function (){},
+            data: TestHelper.newData(new Data())
+
+        };
+        mountedForm = undefined;
+    });
+
+    it("always renders a Button", () => {
+        TestHelper.sizeEqualWithFindAndLength(pubForm, "Button", 1)
+    });
+
+    it("always renders a shit", () => {
+        let form = shallow(<PublicationForm {...formProps} />);
+        let wrapper = pubForm();
+        wrapper.setState({modalOpen: true});
+        TestHelper.sizeEqualWithFindAndLength(wrapper, "Button", 5);
+    });
+
+
+
+});
