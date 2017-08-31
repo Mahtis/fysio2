49c889199a2111604f3435d69fc25d06ce5fd7e0
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2017-08-31 22:12:23 +0300

    CategoryForm-test

--- app/javascript/Components/__tests__/CategoryForm-test.js
+++ app/javascript/Components/__tests__/CategoryForm-test.js
@@ -1,3 +1,48 @@
-/**
- * Created by perkoila on 31.8.2017.
- */
+import React from "react";
+import { shallow, mount } from "enzyme";
+import CategoryForm from '../NavBar/CategoryForm';
+import TestHelper from '../Helpers/Tests.js';
+import Data from '../../Services/Data';
+
+describe("PublicationForm", () => {
+    let props;
+    let mountedForm;
+
+    let states = {
+        name: "",
+        abstract: "",
+        year: "",
+        journal: "",
+        categories: [],
+        authors: [],
+        authorField: "",
+        authorSelected: [],
+        links: [],
+        linkField: "",
+        linkSelect: "",
+        linkSelected: [],
+        modalOpen: false,
+        collapseLink: false,
+        collapseAuthor: false
+    };
+
+    let pubForm = () => {
+        mountedForm = TestHelper.initializationWithMount(mountedForm, <PublicationForm {...props} />);
+        return mountedForm;
+    };
+
+    let pubFormShallow = () => {
+        mountedForm = TestHelper.initializationWithShallow(mountedForm, <PublicationForm {...props} />);
+        return mountedForm;
+    };
+
+    beforeEach(() => {
+        props = {
+            createPublication: function(){},
+            createCategory: function (){},
+            data: TestHelper.newData(new Data())
+
+        };
+        mountedForm = undefined;
+    });
+});
